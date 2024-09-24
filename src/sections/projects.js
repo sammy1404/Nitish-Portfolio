948e5678fbedb02e6a05478d74da299171a4e3ac
Sam Susikar <samsusikar@MacBook-Pro.local>
2024-09-24 13:21:09 +0530

    projects done

--- src/sections/projects.js
+++ src/sections/projects.js
@@ -20,22 +20,38 @@ const projects = () => {
                     achieving state-of-the-art results on MoleculeNet benchmarks.
                     </li>
                 </ul>
+                <div className='projectsRow'>
+                <a href='https://github.com/Deceptrax123/Molecule-Chemical-Property-Prediction-using-Graph-Isomorphism-and-Adversarial-Pretraining'>Molecular Property Prediction code</a>
+                <a href='https://github.com/Deceptrax123/Defending-Graph-Neural-Networks-against-Adversarial-Attacks'>Adversarial attack defence code</a>
+                </div>
+                
             </div>
-            <div class="flex-item">
-                <div className='projectHeader'>
+            <div className="flex-item">
+                <div className="projectHeader">
                     <h2>Real Time Surgical Smoke Detection using Graph Neural Networks and 3D CNNs</h2>
                 </div>
-                <ul className='projectBullets'>
+                <ul className="projectBullets">
                     <li>
-                    Designed a feature extraction system combining a Graph Neural Network and a 3D Convolutional Neural 
+                    Designed a feature extraction system combining a Graph Neural Network and a 3D Convolutional Neural
                     Network to detect smoke in surgical videos.
                     </li>
                     <li>
-                    Used temporal frame positions to form the Graph Adjacency Matrix, exploring a broad range of features 
+                    Used temporal frame positions to form the Graph Adjacency Matrix, exploring a broad range of features
                     dynamically during training.
                     </li>
                 </ul>
+                <div className='githubLogo'>
+                    <a href='https://github.com/Deceptrax123/Real-Time-Smoke-Detection-during-Laproscopy-using-surgical-videos'>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#000000" className="github" viewBox="0 0 16 16">
+                            <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8"/>
+                        </svg>
+                    </a>
+                </div>
+                
+                
+                
             </div>
+
             <div class="flex-item">
                 <div className='projectHeader'>
                     <h2>Differential Equations using Equilibrium Driven Neural Networks</h2>
@@ -50,6 +66,14 @@ const projects = () => {
                     Conditions for Partial Differential Equations.
                     </li>
                 </ul>
+                <div className='githubLogo'>
+                    <a href='github.com/deceptrax123/differential-equations-using-neural-networks'>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#000000" className="github" viewBox="0 0 16 16">
+                            <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8"/>
+                        </svg>
+                    </a>
+
+                </div>
             </div>
             <div class="flex-item">
                 <div className='projectHeader'>
@@ -61,6 +85,14 @@ const projects = () => {
                     transfer the pre-trained backbone weights to Ultralytics YOLOv5 for fine-tuning.
                     </li>
                 </ul>
+                <div className='githubLogo'>
+                    <a href='https://github.com/Deceptrax123/Self-Supervised-Learning-using-YOLOv5-Backbone'>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#000000" className="github" viewBox="0 0 16 16">
+                            <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8"/>
+                        </svg>
+                    </a>
+
+                </div>
             </div>
             <div class="flex-item">
                 <div className='projectHeader'>
@@ -72,6 +104,14 @@ const projects = () => {
                     including Image Inpainting and Weighted Box Fusion (WBF).
                     </li>
                 </ul>
+                <div className='githubLogo'>
+                    <a href='https://github.com/Deceptrax123/Spurious-Correlation-Mitigation-in-YOLOv5'>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#000000" className="github" viewBox="0 0 16 16">
+                            <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8"/>
+                        </svg>
+                    </a>
+
+                </div>
             </div>
             <div class="flex-item">
                 <div className='projectHeader'>
@@ -83,6 +123,14 @@ const projects = () => {
                     generators and 2 discriminators to maintain cycle consistencies.
                     </li>
                 </ul>
+                <div className='githubLogo'>
+                    <a href='https://github.com/Deceptrax123/Deep-Learning-Based-Medical-Image-Encryption-and-Decryption-using-GANs'>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#000000" className="github" viewBox="0 0 16 16">
+                            <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8"/>
+                        </svg>
+                    </a>
+
+                </div>
             </div>
             <div class="flex-item">
                 <div className='projectHeader'>
@@ -98,6 +146,14 @@ const projects = () => {
                     its reflectance images using the reflectances of each band against both surface and plastic.
                     </li>
                 </ul>
+                <div className='githubLogo'>
+                    <a href='https://github.com/Deceptrax123/Plastic-Detection-using-Reflectance-Images-of-Hyperspectral-Bands'>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#000000" className="github" viewBox="0 0 16 16">
+                            <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8"/>
+                        </svg>
+                    </a>
+
+                </div>
             </div>
             <div class="flex-item">
                 <div className='projectHeader'>
@@ -110,6 +166,14 @@ const projects = () => {
                     hackathon.
                     </li>
                 </ul>
+                <div className='githubLogo'>
+                    <a href='https://github.com/Deceptrax123/Tamil-STT-using-Spectrogram-Analysis'>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#000000" className="github" viewBox="0 0 16 16">
+                            <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8"/>
+                        </svg>
+                    </a>
+
+                </div>
             </div>
         </div>
 
